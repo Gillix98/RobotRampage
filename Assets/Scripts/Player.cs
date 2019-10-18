0726c0fc25973ebe1a9f8b7be28a4591f92b2c11
Gavin Crews [Student] <gavin.crews@student.sl.on.ca>
2019-10-18 13:03:25 -0400

    finished adding missiles and adding damage effects

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public int armor;
+    public GameUI gameUI;
+    private GunEquipper gunEquipper;
+    private Ammo ammo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
