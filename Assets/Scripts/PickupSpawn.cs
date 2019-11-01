f033f2249af3f27f05f35e0d0e74d10d0ab88988
Gavin Crews [Student] <gavin.crews@Student.SL.On.Ca>
2019-11-01 13:55:32 -0400

    finished pickup spawns and starting robot spawns

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    //1
+     void spawnPickups()
+    {
+        // instantiate a random pickup
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    //2
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickups();
+    }
+
+    //3
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPickups(); 
+    }
+
+    //4
+    // Update is called once per frame
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup"); 
+    }
+}
